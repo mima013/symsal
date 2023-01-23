7f192345c4541dd44cded6bc2f6cff6552321617
mima013 <mimaberekia@gmail.com>
2023-01-23 02:06:27 +0200

    deuxieme version
    class= paiement

--- js/main.js
+++ js/main.js
@@ -100,3 +100,21 @@
     
 })(jQuery);
 
+/*un code interface effectuer payement*/
+$(function(){
+    $("#form-total").steps({
+        headerTag: "h2",
+        bodyTag: "section",
+        transitionEffect: "fade",
+        enableAllSteps: true,
+        autoFocus: true,
+        transitionEffectSpeed: 500,
+        titleTemplate : '<span class="title">#title#</span>',
+        labels: {
+            previous : 'Precedant',
+            next : 'Suivant',
+            finish : 'Confirm',
+           
+        }
+    });
+});
